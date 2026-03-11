56f65078bf12ded871977222fb4ebd970aaf18f8
Siyabonga Mgwenya <150593421+Kaybee574@users.noreply.github.com>
2026-03-11 12:25:44 +0200

    Current Version of RU thrifty
    Used old code from RAGA in most pages but changed the styling to be more consistent, some pages broke(not completely, the height of most pages conflicts with the rest of the index.html)

---
+++ category_pictures/Books/carousel.js
@@ -0,0 +1,49 @@
+// this is where i got this from https://www.youtube.com/watch?v=749ta0nvj8s
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+
+initializeSlider();
+function initializeSlider()
+{
+    if(slides.length>0)
+    {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide,1000);        
+    }
+}
+
+function showSlide(index)
+{
+    if(index >= slides.length)
+    {
+        slideIndex = 0;
+    }
+
+    else if(index < 0)
+    {
+        slideIndex = slides.length -1;
+    }
+    slides.forEach(slide =>
+    {
+        slide.classList.remove("displaySlide");
+
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide()
+{
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide()
+{
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+}
